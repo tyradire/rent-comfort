6fe8ba413d78f0b2a13b7efcefa4cee89b23d8b8
Daniel Ugarov <tyradire@yandex.ru>
2024-01-03 14:01:54 +0300

    feat: add filtering to the arenda page

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -1,35 +1,10 @@
+// dev settings
+let isDev = window.location.hostname == 'localhost';
+
 // Фикс паддинга скролла страниц, которые умещаются на экране
 if ($(document).height() <= $(window).height()) {
   $(document.body).css('padding-right', '17px');
 }
-
-// Отображение списка апартаментов
-let apartmentsArenda = $('#apartments-list-arenda');
-let apartmentsData = apartmentsList.map(elem => elem.address);
-
-$.each(apartmentsList, function( index, apartments ) {
-  $(
-    `
-    <li class="apartments-list-item" id='${apartments.id}'>
-    <img src="${apartments.photo}">
-    <div class="apartments-list-item-info">
-      <a href='./detail.html'>
-        <p class="apartments-list-item-title">${apartments.address}</p>
-      </a>
-      <ul class="apartments-list-item-tags">
-        <li class="apartments-list-item-tag">2-комн.</li>
-        <li class="apartments-list-item-tag">wi-fi</li>
-        <li class="apartments-list-item-tag">лифт</li>
-        <li class="apartments-list-item-tag">3-эт</li>
-        <li class="apartments-list-item-tag">парковка</li>
-      </ul>
-      <p class="apartments-list-item-status">Доступно <span class="apartments-list-item-status-date">без ограничений</span></p>
-      <p class="apartments-list-item-price">от <span class="apartments-list-item-price-sum">30.000</span> &#8381;/мес</p>
-    </div>
-    </li>
-    ` 
-  ).appendTo(apartmentsArenda);
-});
 
 // Псевдомаршрутизация с помощью localstorage
 $('.apartments-list-item').each(function(index) {
@@ -43,17 +18,57 @@ $('.apartments-list-item').each(function(index) {
   )
 })
 
-if (window.location.pathname == '/detail.html') {
+let searchFormData = JSON.parse(localStorage.getItem('searchFormData'));
+
+if (window.location.pathname == '/detail.html' || isDev) {
   let currentId = Number(localStorage.getItem('currentApartments'));
   let currentApartments = apartmentsList.find(apartments => apartments.id === currentId)
   $('.detail-booking-title').text('Апартаменты на ' + currentApartments.address)
   $('.detail-booking-city').text(currentApartments.city)
 }
 
-if (window.location.pathname == '/arenda.html') {
-  let searchFormData = JSON.parse(localStorage.getItem('searchFormData'));
+if (window.location.pathname == '/arenda.html' || isDev) {
   $('#main-form-search-input').val(searchFormData.city);
   $('#main-form-datepicker-in').val(searchFormData.dateIn);
   $('#main-form-datepicker-out').val(searchFormData.dateOut);
   $('#main-search-form-counter').val(searchFormData.guests);
 }
+
+// Отображение списка апартаментов
+let apartmentsArenda = $('#apartments-list-arenda');
+let apartmentsData = apartmentsList.map(elem => elem.address);
+let filtredApartmentsList = apartmentsList.filter(city => city.cityCode ==  searchFormData.city);
+
+let addItemsToList = (selectedCity) => {
+  $.each(selectedCity, function( index, apartments ) {
+    $(
+      `
+      <li class="apartments-list-item" id='${apartments.id}'>
+      <img src="${apartments.photo}">
+      <div class="apartments-list-item-info">
+        <a href='./detail.html'>
+          <p class="apartments-list-item-title">${apartments.address}</p>
+        </a>
+        <ul class="apartments-list-item-tags">
+          <li class="apartments-list-item-tag">2-комн.</li>
+          <li class="apartments-list-item-tag">wi-fi</li>
+          <li class="apartments-list-item-tag">лифт</li>
+          <li class="apartments-list-item-tag">3-эт</li>
+          <li class="apartments-list-item-tag">парковка</li>
+        </ul>
+        <p class="apartments-list-item-status">Доступно <span class="apartments-list-item-status-date">без ограничений</span></p>
+        <p class="apartments-list-item-price">от <span class="apartments-list-item-price-sum">30.000</span> &#8381;/мес</p>
+      </div>
+      </li>
+      ` 
+    ).appendTo(apartmentsArenda);
+  });
+}
+
+addItemsToList(filtredApartmentsList);
+
+$('#main-form-search-input').on('change', function() {
+  $('#apartments-list-arenda').children().remove();
+  filtredApartmentsList = apartmentsList.filter(city => city.cityCode ==  $('#main-form-search-input').val());
+  addItemsToList(filtredApartmentsList);
+})
--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,8 +1,9 @@
 const apartmentsList = [
-  // MSK
+  // MSC
     {
       id: 1,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.768,37.604],
       address: 'Пл. Ленина 1',
       phone: '+791234567890',
@@ -12,6 +13,7 @@ const apartmentsList = [
     {
       id: 2,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.778,37.595],
       address: 'Пл. Ленина 2',
       phone: '+791234567890',
@@ -21,6 +23,7 @@ const apartmentsList = [
     {
       id: 3,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.744,37.589],
       address: 'Пл. Ленина 3',
       phone: '+791234567890',
@@ -30,6 +33,7 @@ const apartmentsList = [
     {
       id: 4,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.757,37.650],
       address: 'Пл. Ленина 4',
       phone: '+791234567890',
@@ -39,6 +43,7 @@ const apartmentsList = [
     {
       id: 5,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.770,37.603],
       address: 'Пл. Ленина 5',
       phone: '+791234567890',
@@ -48,6 +53,7 @@ const apartmentsList = [
     {
       id: 6,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.746,37.589],
       address: 'Пл. Ленина 6',
       phone: '+791234567890',
@@ -57,6 +63,7 @@ const apartmentsList = [
     {
       id: 7,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.733,37.619],
       address: 'Пл. Ленина 7',
       phone: '+791234567890',
@@ -66,6 +73,7 @@ const apartmentsList = [
     {
       id: 8,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.767,37.572],
       address: 'Пл. Ленина 8',
       phone: '+791234567890',
@@ -75,6 +83,7 @@ const apartmentsList = [
     {
       id: 9,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.750,37.675],
       address: 'Пл. Ленина 9',
       phone: '+791234567890',
@@ -84,6 +93,7 @@ const apartmentsList = [
     {
       id: 10,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.737,37.635],
       address: 'Пл. Ленина 10',
       phone: '+791234567890',
@@ -93,6 +103,7 @@ const apartmentsList = [
     {
       id: 11,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.756,37.563],
       address: 'Пл. Ленина 11',
       phone: '+791234567890',
@@ -102,6 +113,7 @@ const apartmentsList = [
     {
       id: 12,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.730,37.583],
       address: 'Пл. Ленина 12',
       phone: '+791234567890',
@@ -111,6 +123,7 @@ const apartmentsList = [
     {
       id: 13,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.779,37.611],
       address: 'Пл. Ленина 13',
       phone: '+791234567890',
@@ -120,6 +133,7 @@ const apartmentsList = [
     {
       id: 14,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.788,37.596],
       address: 'Пл. Ленина 14',
       phone: '+791234567890',
@@ -129,6 +143,7 @@ const apartmentsList = [
     {
       id: 15,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.763,37.572],
       address: 'Пл. Ленина 15',
       phone: '+791234567890',
@@ -138,6 +153,7 @@ const apartmentsList = [
     {
       id: 16,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.755,37.637],
       address: 'Пл. Ленина 16',
       phone: '+791234567890',
@@ -147,6 +163,7 @@ const apartmentsList = [
     {
       id: 17,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.775,37.628],
       address: 'Пл. Ленина 17',
       phone: '+791234567890',
@@ -156,6 +173,7 @@ const apartmentsList = [
     {
       id: 18,
       city: 'Москва',
+      cityCode: 'MSC',
       coords: [55.749,37.589],
       address: 'Пл. Ленина 18',
       phone: '+791234567890',
@@ -167,6 +185,7 @@ const apartmentsList = [
     {
       id: 19,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.988,30.343],
       address: 'Пл. Ленина 19',
       phone: '+791234567890',
@@ -176,6 +195,7 @@ const apartmentsList = [
     {
       id: 20,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.951,30.238],
       address: 'Пл. Ленина 20',
       phone: '+791234567890',
@@ -185,6 +205,7 @@ const apartmentsList = [
     {
       id: 21,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.928,30.328],
       address: 'Пл. Ленина 21',
       phone: '+791234567890',
@@ -194,6 +215,7 @@ const apartmentsList = [
     {
       id: 22,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.914,30.278],
       address: 'Пл. Ленина 22',
       phone: '+791234567890',
@@ -203,6 +225,7 @@ const apartmentsList = [
     {
       id: 23,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.958,30.293],
       address: 'Пл. Ленина 23',
       phone: '+791234567890',
@@ -212,6 +235,7 @@ const apartmentsList = [
     {
       id: 24,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.943,30.271],
       address: 'Пл. Ленина 24',
       phone: '+791234567890',
@@ -221,6 +245,7 @@ const apartmentsList = [
     {
       id: 25,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.928,30.311],
       address: 'Пл. Ленина 25',
       phone: '+791234567890',
@@ -230,6 +255,7 @@ const apartmentsList = [
     {
       id: 26,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.942,30.356],
       address: 'Пл. Ленина 26',
       phone: '+791234567890',
@@ -239,6 +265,7 @@ const apartmentsList = [
     {
       id: 27,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.961,30.321],
       address: 'Пл. Ленина 27',
       phone: '+791234567890',
@@ -248,6 +275,7 @@ const apartmentsList = [
     {
       id: 28,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.973,30.302],
       address: 'Пл. Ленина 28',
       phone: '+791234567890',
@@ -257,6 +285,7 @@ const apartmentsList = [
     {
       id: 29,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.973,30.265],
       address: 'Пл. Ленина 29',
       phone: '+791234567890',
@@ -266,6 +295,7 @@ const apartmentsList = [
     {
       id: 30,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.970,30.265],
       address: 'Пл. Ленина 31',
       phone: '+791234567890',
@@ -275,6 +305,7 @@ const apartmentsList = [
     {
       id: 31,
       city: 'Санкт-Петербург',
+      cityCode: 'SPB',
       coords: [59.989,30.252],
       address: 'Пл. Ленина 32',
       phone: '+791234567890',
@@ -285,6 +316,7 @@ const apartmentsList = [
     {
       id: 32,
       city: 'Новосибирск',
+      cityCode: 'NSK',
       coords: [55.029,82.926],
       address: 'Пл. Ленина 33',
       phone: '+791234567890',
@@ -294,6 +326,7 @@ const apartmentsList = [
     {
       id: 33,
       city: 'Новосибирск',
+      cityCode: 'NSK',
       coords: [55.046,82.983],
       address: 'Пл. Ленина 34',
       phone: '+791234567890',
@@ -303,6 +336,7 @@ const apartmentsList = [
     {
       id: 34,
       city: 'Новосибирск',
+      cityCode: 'NSK',
       coords: [55.019,82.957],
       address: 'Пл. Ленина 35',
       phone: '+791234567890',
@@ -312,6 +346,7 @@ const apartmentsList = [
     {
       id: 35,
       city: 'Новосибирск',
+      cityCode: 'NSK',
       coords: [55.061,82.921],
       address: 'Пл. Ленина 36',
       phone: '+791234567890',
@@ -321,6 +356,7 @@ const apartmentsList = [
     {
       id: 36,
       city: 'Новосибирск',
+      cityCode: 'NSK',
       coords: [54.992,82.859],
       address: 'Пл. Ленина 37',
       phone: '+791234567890',
@@ -330,6 +366,7 @@ const apartmentsList = [
     {
       id: 37,
       city: 'Новосибирск',
+      cityCode: 'NSK',
       coords: [54.965,82.883],
       address: 'Пл. Ленина 38',
       phone: '+791234567890',
@@ -340,6 +377,7 @@ const apartmentsList = [
     {
       id: 38,
       city: 'Екатеринбург',
+      cityCode: 'EKB',
       coords: [56.867,60.601],
       address: 'Пл. Ленина 39',
       phone: '+791234567890',
@@ -349,6 +387,7 @@ const apartmentsList = [
     {
       id: 39,
       city: 'Екатеринбург',
+      cityCode: 'EKB',
       coords: [56.850,60.608],
       address: 'Пл. Ленина 40',
       phone: '+791234567890',
@@ -358,6 +397,7 @@ const apartmentsList = [
     {
       id: 40,
       city: 'Екатеринбург',
+      cityCode: 'EKB',
       coords: [56.829,60.587],
       address: 'Пл. Ленина 41',
       phone: '+791234567890',
@@ -367,6 +407,7 @@ const apartmentsList = [
     {
       id: 41,
       city: 'Екатеринбург',
+      cityCode: 'EKB',
       coords: [56.823,60.597],
       address: 'Пл. Ленина 42',
       phone: '+791234567890',
@@ -376,6 +417,7 @@ const apartmentsList = [
     {
       id: 42,
       city: 'Екатеринбург',
+      cityCode: 'EKB',
       coords: [56.832,60.625],
       address: 'Пл. Ленина 43',
       phone: '+791234567890',
@@ -385,6 +427,7 @@ const apartmentsList = [
     {
       id: 43,
       city: 'Екатеринбург',
+      cityCode: 'EKB',
       coords: [56.834,60.572],
       address: 'Пл. Ленина 44',
       phone: '+791234567890',
@@ -394,6 +437,7 @@ const apartmentsList = [
     {
       id: 44,
       city: 'Екатеринбург',
+      cityCode: 'EKB',
       coords: [56.893,60.623],
       address: 'Пл. Ленина 45',
       phone: '+791234567890',
@@ -404,6 +448,7 @@ const apartmentsList = [
     {
       id: 45,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.788,49.156],
       address: 'Пл. Ленина 46',
       phone: '+791234567890',
@@ -413,6 +458,7 @@ const apartmentsList = [
     {
       id: 46,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.790,49.147],
       address: 'Пл. Ленина 47',
       phone: '+791234567890',
@@ -422,6 +468,7 @@ const apartmentsList = [
     {
       id: 47,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.784,49.132],
       address: 'Пл. Ленина 48',
       phone: '+791234567890',
@@ -431,6 +478,7 @@ const apartmentsList = [
     {
       id: 48,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.790,49.119],
       address: 'Пл. Ленина 49',
       phone: '+791234567890',
@@ -440,6 +488,7 @@ const apartmentsList = [
     {
       id: 49,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.793,49.116],
       address: 'Пл. Ленина 50',
       phone: '+791234567890',
@@ -449,6 +498,7 @@ const apartmentsList = [
     {
       id: 50,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.794,49.099],
       address: 'Пл. Ленина 51',
       phone: '+791234567890',
@@ -458,6 +508,7 @@ const apartmentsList = [
     {
       id: 51,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.819,49.080],
       address: 'Пл. Ленина 52',
       phone: '+791234567890',
@@ -467,6 +518,7 @@ const apartmentsList = [
     {
       id: 52,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.825,49.100],
       address: 'Пл. Ленина 53',
       phone: '+791234567890',
@@ -476,6 +528,7 @@ const apartmentsList = [
     {
       id: 53,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.830,49.135],
       address: 'Пл. Ленина 54',
       phone: '+791234567890',
@@ -485,6 +538,7 @@ const apartmentsList = [
     {
       id: 54,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.835,49.075],
       address: 'Пл. Ленина 55',
       phone: '+791234567890',
@@ -494,6 +548,7 @@ const apartmentsList = [
     {
       id: 55,
       city: 'Казань',
+      cityCode: 'KAZ',
       coords: [55.821,49.149],
       address: 'Пл. Ленина 56',
       phone: '+791234567890',
@@ -504,6 +559,7 @@ const apartmentsList = [
     {
       id: 56,
       city: 'Нижний Новгород',
+      cityCode: 'NNG',
       coords: [56.323,44.021],
       address: 'Пл. Ленина 57',
       phone: '+791234567890',
@@ -513,6 +569,7 @@ const apartmentsList = [
     {
       id: 57,
       city: 'Нижний Новгород',
+      cityCode: 'NNG',
       coords: [56.328,43.937],
       address: 'Пл. Ленина 58',
       phone: '+791234567890',
@@ -522,6 +579,7 @@ const apartmentsList = [
     {
       id: 58,
       city: 'Нижний Новгород',
+      cityCode: 'NNG',
       coords: [56.310,43.938],
       address: 'Пл. Ленина 59',
       phone: '+791234567890',
@@ -531,6 +589,7 @@ const apartmentsList = [
     {
       id: 59,
       city: 'Нижний Новгород',
+      cityCode: 'NNG',
       coords: [56.312,43.995],
       address: 'Пл. Ленина 60',
       phone: '+791234567890',
@@ -541,6 +600,7 @@ const apartmentsList = [
     {
       id: 60,
       city: 'Челябинск',
+      cityCode: 'CHB',
       coords: [55.227,61.407],
       address: 'Пл. Ленина 61',
       phone: '+791234567890',
@@ -550,6 +610,7 @@ const apartmentsList = [
     {
       id: 61,
       city: 'Челябинск',
+      cityCode: 'CHB',
       coords: [55.188,61.424],
       address: 'Пл. Ленина 62',
       phone: '+791234567890',
@@ -559,6 +620,7 @@ const apartmentsList = [
     {
       id: 62,
       city: 'Челябинск',
+      cityCode: 'CHB',
       coords: [55.177,61.431],
       address: 'Пл. Ленина 63',
       phone: '+791234567890',
@@ -568,6 +630,7 @@ const apartmentsList = [
     {
       id: 63,
       city: 'Челябинск',
+      cityCode: 'CHB',
       coords: [55.178,61.373],
       address: 'Пл. Ленина 64',
       phone: '+791234567890',
@@ -577,6 +640,7 @@ const apartmentsList = [
     {
       id: 64,
       city: 'Челябинск',
+      cityCode: 'CHB',
       coords: [55.159,61.399],
       address: 'Пл. Ленина 65',
       phone: '+791234567890',
@@ -586,6 +650,7 @@ const apartmentsList = [
     {
       id: 65,
       city: 'Челябинск',
+      cityCode: 'CHB',
       coords: [55.214,61.404],
       address: 'Пл. Ленина 66',
       phone: '+791234567890',
@@ -596,6 +661,7 @@ const apartmentsList = [
     {
       id: 66,
       city: 'Самара',
+      cityCode: 'SAM',
       coords: [53.195,50.130],
       address: 'Пл. Ленина 67',
       phone: '+791234567890',
@@ -605,6 +671,7 @@ const apartmentsList = [
     {
       id: 67,
       city: 'Самара',
+      cityCode: 'SAM',
       coords: [53.190,50.161],
       address: 'Пл. Ленина 68',
       phone: '+791234567890',
@@ -614,6 +681,7 @@ const apartmentsList = [
     {
       id: 68,
       city: 'Самара',
+      cityCode: 'SAM',
       coords: [53.210,50.187],
       address: 'Пл. Ленина 69',
       phone: '+791234567890',
@@ -624,6 +692,7 @@ const apartmentsList = [
     {
       id: 69,
       city: 'Уфа',
+      cityCode: 'UFA',
       coords: [54.738,55.983],
       address: 'Пл. Ленина 70',
       phone: '+791234567890',
@@ -633,6 +702,7 @@ const apartmentsList = [
     {
       id: 70,
       city: 'Уфа',
+      cityCode: 'UFA',
       coords: [54.734,55.946],
       address: 'Пл. Ленина 71',
       phone: '+791234567890',
@@ -642,6 +712,7 @@ const apartmentsList = [
     {
       id: 71,
       city: 'Уфа',
+      cityCode: 'UFA',
       coords: [54.726,55.962],
       address: 'Пл. Ленина 72',
       phone: '+791234567890',
@@ -651,6 +722,7 @@ const apartmentsList = [
     {
       id: 72,
       city: 'Уфа',
+      cityCode: 'UFA',
       coords: [54.745,55.992],
       address: 'Пл. Ленина 73',
       phone: '+791234567890',
@@ -661,6 +733,7 @@ const apartmentsList = [
     {
       id: 73,
       city: 'Ростов-на-Дону',
+      cityCode: 'RND',
       coords: [47.228,39.709],
       address: 'Пл. Ленина 74',
       phone: '+791234567890',
@@ -670,6 +743,7 @@ const apartmentsList = [
     {
       id: 74,
       city: 'Ростов-на-Дону',
+      cityCode: 'RND',
       coords: [47.223,39.700],
       address: 'Пл. Ленина 75',
       phone: '+791234567890',
@@ -679,6 +753,7 @@ const apartmentsList = [
     {
       id: 75,
       city: 'Ростов-на-Дону',
+      cityCode: 'RND',
       coords: [47.221,39.676],
       address: 'Пл. Ленина 76',
       phone: '+791234567890',
@@ -688,6 +763,7 @@ const apartmentsList = [
     {
       id: 76,
       city: 'Ростов-на-Дону',
+      cityCode: 'RND',
       coords: [47.227,39.677],
       address: 'Пл. Ленина 77',
       phone: '+791234567890',
@@ -697,6 +773,7 @@ const apartmentsList = [
     {
       id: 77,
       city: 'Ростов-на-Дону',
+      cityCode: 'RND',
       coords: [47.218,39.656],
       address: 'Пл. Ленина 78',
       phone: '+791234567890',
@@ -707,6 +784,7 @@ const apartmentsList = [
     {
       id: 78,
       city: 'Омск',
+      cityCode: 'OMS',
       coords: [54.993,73.374],
       address: 'Пл. Ленина 79',
       phone: '+791234567890',
@@ -716,6 +794,7 @@ const apartmentsList = [
     {
       id: 79,
       city: 'Омск',
+      cityCode: 'OMS',
       coords: [54.997,73.360],
       address: 'Пл. Ленина 80',
       phone: '+791234567890',
@@ -725,6 +804,7 @@ const apartmentsList = [
     {
       id: 80,
       city: 'Омск',
+      cityCode: 'OMS',
       coords: [54.987,73.366],
       address: 'Пл. Ленина 81',
       phone: '+791234567890',
@@ -735,6 +815,7 @@ const apartmentsList = [
     {
       id: 81,
       city: 'Красноярск',
+      cityCode: 'KRK',
       coords: [56.014,92.859],
       address: 'Пл. Ленина 82',
       phone: '+791234567890',
@@ -744,6 +825,7 @@ const apartmentsList = [
     {
       id: 82,
       city: 'Красноярск',
+      cityCode: 'KRK',
       coords: [56.024,92.874],
       address: 'Пл. Ленина 83',
       phone: '+791234567890',
@@ -754,6 +836,7 @@ const apartmentsList = [
     {
       id: 83,
       city: 'Воронеж',
+      cityCode: 'VRN',
       coords: [51.683,39.180],
       address: 'Пл. Ленина 84',
       phone: '+791234567890',
@@ -763,6 +846,7 @@ const apartmentsList = [
     {
       id: 84,
       city: 'Воронеж',
+      cityCode: 'VRN',
       coords: [51.690,39.191],
       address: 'Пл. Ленина 85',
       phone: '+791234567890',
@@ -772,6 +856,7 @@ const apartmentsList = [
     {
       id: 85,
       city: 'Воронеж',
+      cityCode: 'VRN',
       coords: [51.689,39.212],
       address: 'Пл. Ленина 86',
       phone: '+791234567890',
@@ -781,6 +866,7 @@ const apartmentsList = [
     {
       id: 86,
       city: 'Воронеж',
+      cityCode: 'VRN',
       coords: [51.666,39.199],
       address: 'Пл. Ленина 87',
       phone: '+791234567890',
@@ -790,6 +876,7 @@ const apartmentsList = [
     {
       id: 87,
       city: 'Воронеж',
+      cityCode: 'VRN',
       coords: [51.661,39.190],
       address: 'Пл. Ленина 88',
       phone: '+791234567890',
