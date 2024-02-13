649d08e72c295dfa3f48ae37a67367f272307a98
Daniel Ugarov <tyradire@yandex.ru>
2024-02-13 17:57:07 +0300

    feat: add feedback modal for order form

---
+++ src/scripts/modals.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  $("#order-landlord-feedback-button").on("click", function() {
+    $(".order .cover").css({
+      "opacity": "1", 
+      "transform": "translateX(0)"
+    });
+    $(".order .cover-modal").css({
+      "background-color": "rgba(0, 0, 0, 0.25)",
+      "opacity": "1",
+      "visibility": "visible"
+    });
+    $("body").css({
+      "overflow": "hidden",
+      "margin-right": "17px"
+    });
+  });
+  $(".order .form.modal .input-close").on("click", function() {
+    $(".order .cover").css({
+      "opacity": "0",
+      "transform": "translateX(100%)"
+    });
+    $(".order .cover-modal").css({
+      "background-color": "unset",
+      "opacity": "0",
+      "visibility": "hidden"
+    });
+    $("body").css({
+      "overflow": "unset",
+      "margin-right": "0"
+    });
+  });
+});
